c25113add1a8fcbe4132e2bcd0eb1ee6b7c275e2
ChiaChengKuo <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-05-07 15:59:12 -0700

    work on react front end

---
+++ restaurant-app/src/components/Order/OrderForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Form from "../../layouts/Form"
+
+export default function OrderForm() {
+  return (
+    <form noValidate autoComplete='off'>
+        <Form>
+            <Grid>
+                
+            </Grid>
+        </Form>
+    </form>
+  )
+}
