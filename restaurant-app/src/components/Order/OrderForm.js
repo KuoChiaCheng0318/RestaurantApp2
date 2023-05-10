931c1ebd0da35999b838c0b9abda8fa6c1540efa
ChiaChengKuo <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-05-10 16:44:10 -0700

    Finished Form

--- restaurant-app/src/components/Order/OrderForm.js
+++ restaurant-app/src/components/Order/OrderForm.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react'
 import Form from "../../layouts/Form"
 import { Grid, InputAdornment, makeStyles, ButtonGroup, Button as MuiButton } from '@material-ui/core';
+import ReplayIcon from '@material-ui/icons/Replay';
+import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu';
+import ReorderIcon from '@material-ui/icons/Reorder';
 import { Input, Select, Button } from "../../controls";
 import { NoEncryption } from '@material-ui/icons';
+import { useForm } from '../../hooks/useForm';
 
 const pMethods = [
   { id: 'none', title: 'Select' },
@@ -10,75 +14,87 @@ const pMethods = [
   { id: 'Card', title: 'Card' },
 ]
 
-const generateOrderNumber = () => Math.floor(100000+ Math.random() * 900000).toString();
-
-const getFreshModelObject= () => ({
-  orderMasterId:0,
-  orderNumber: generateOrderNumber(),
-  customerId: 0,
-  pMethod: 'none',
-  gTotal: 0,
-  deletedOrderItemIds:'',
-  orderDetails: []
-})
-
-
-export default function OrderForm() {
-
-  const [values, setValues]=useState(getFreshModelObject());
-  const handleInputChange = e => {
-    const {name, value}= e.target;
-    setValues({
-      ...values,
-      [name]: value
-    })
+const useStyles = makeStyles(theme => ({
+  adornmentText: {
+      '& .MuiTypography-root': {
+          color: '#f3b33d',
+          fontWeight: 'bolder',
+          fontSize: '1.5em'
+      }
+  },
+  submitButtonGroup: {
+      backgroundColor: '#f3b33d',
+      color: '#000',
+      margin: theme.spacing(1),
+      '& .MuiButton-label': {
+          textTransform: 'none'
+      },
+      '&:hover': {
+          backgroundColor: '#f3b33d',
+      }
   }
-  const resetFormControls= () => {
-    setValues(getFreshModelObject())
-  }
+}))
+
+
+export default function OrderForm(props) {
+
+  const {values, errors, handleInputChange} = props;
+  const classes = useStyles();
 
   return (
-    <form noValidate autoComplete='off'>
-        <Form>
-            <Grid container>
-                <Grid item xs={6}>
-                <Input 
-                  disabled
-                  label="Order Number"
-                  name="orderNumber"
-                  value={values.orderNumber}
-                />
-                <Select
-                  label="Customer"
-                  name="customerId"
-                  value={values.customerId}
-                  onChange= {handleInputChange}
-                  options={[
-                    {id:0, title:'Select'},
-                    {id:1, title:'Customer 1'},
-                    {id:2, title:'Customer 2'},
-                    {id:3, title:'Customer 3'},
-                    {id:4, title:'Customer 4'}
-                  ]}
-                />
-                </Grid>
-                <Grid item xs={6}>
-                <Select
-                  label="Payment Method"
-                  name="pMethod"
-                  options={pMethods}
-                  value={values.pMethod}
-                  onChange= {handleInputChange}
-                />
-                <Input 
-                  disabled
-                  label="Grand Total"
-                  name="gTotal"
-                  value={values.gTotal}
-                />
-                </Grid>
-            </Grid>
-        </Form>
-    </form>
+      <Form>
+          <Grid container>
+              <Grid item xs={6}>
+              <Input 
+                disabled
+                label="Order Number"
+                name="orderNumber"
+                value={values.orderNumber}
+                InputProps={{
+                  startAdornment: <InputAdornment
+                      className={classes.adornmentText}
+                      position="start">#</InputAdornment>
+                }}
+              />
+              <Select
+                label="Customer"
+                name="customerId"
+                value={values.customerId}
+                onChange= {handleInputChange}
+                options={[
+                  {id:0, title:'Select'},
+                  {id:1, title:'Customer 1'},
+                  {id:2, title:'Customer 2'},
+                  {id:3, title:'Customer 3'},
+                  {id:4, title:'Customer 4'}
+                ]}
+              />
+              </Grid>
+              <Grid item xs={6}>
+              <Select
+                label="Payment Method"
+                name="pMethod"
+                options={pMethods}
+                value={values.pMethod}
+                onChange= {handleInputChange}
+              />
+              <Input 
+                disabled
+                label="Grand Total"
+                name="gTotal"
+                value={values.gTotal}InputProps={{
+                  startAdornment: <InputAdornment
+                      className={classes.adornmentText}
+                      position="start">$</InputAdornment>
+                }}
+              />
+              <ButtonGroup className={classes.submitButtonGroup}>
+                <MuiButton size="large" endIcon={<RestaurantMenuIcon />} type="submit">Submit</MuiButton>
+                <MuiButton size="small" startIcon={<ReplayIcon />} />
+              </ButtonGroup>
+              <Button size="large" startIcon={<ReorderIcon />}>Orders</Button>
+              </Grid>
+          </Grid>
+      </Form>
   )
 }
